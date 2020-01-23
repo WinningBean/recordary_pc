7421ff43c8bff551caeec9f42c578df9d9259103
wsb0722 <winningbean@gmail.com>
2020-01-23 11:15:18 +0900

    Set basic project
    - Set Spring boot project

---
+++ src/main/java/com/fairy_pitt/recordary/RecordaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.recordary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fairy_pitt/recordary/RecordaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recordary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
