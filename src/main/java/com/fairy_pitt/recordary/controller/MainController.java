9aaf919ec3925423072f130f648b01cff9633e19
wsb0722 <winningbean@gmail.com>
2020-01-31 15:35:14 +0900

    Update MainController.java
    - 로그인 시 JSON 다양하게 보내기

--- src/main/java/com/fairy_pitt/recordary/controller/MainController.java
+++ src/main/java/com/fairy_pitt/recordary/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.fairy_pitt.recordary.controller;
 
+import com.fairy_pitt.recordary.model.Users;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 public class MainController {
 
     @Autowired
@@ -34,11 +37,45 @@ public class MainController {
 
     @GetMapping("/logout")
     public String logout(){
-        session.invalidate();
+//        session.invalidate();
+        session.removeAttribute("loginUser");
         return "index";
     }
 
+    @ResponseBody
+    @GetMapping(value = "/profile")
+    public Map<String, Object> profileRequest(){
+        Map<String, Object> map = new HashMap<>();
+
+        Users currentUser = (Users)session.getAttribute("loginUser");
+        if (currentUser == null) map.put("currentUser","none");
+        else{
+            Map<String, Object> userMap = new HashMap<>();
+            map.put("currentUser", userMap);
+            userMap.put("userId", currentUser.getUserId());
+            userMap.put("userNm", currentUser.getUserNm());
+            userMap.put("userEx", currentUser.getUserEx());
+
+            Map<String, Object> groupMap = new HashMap<>();
+            List groupList = new ArrayList();
+            groupList.add("그룹1");
+            groupList.add("그룹2");
+            groupList.add("그룹3");
+
+            map.put("userGroup", groupMap);
+            groupMap.put("groupCount", 3);
+            for (int i = 0; i < groupList.size(); i++){
+                Map<String, Object> groupDetailMap = new HashMap<>();
+                groupMap.put("group" + (i+1), groupDetailMap);
+                groupDetailMap.put("groupPic", "none");
+                groupDetailMap.put("groupNm", (String)groupList.get(i));
+            }
+        }
+        return map;
+    }
+
 //    @CrossOrigin
+    @ResponseBody
     @GetMapping(value = "/test")
     public Map<String, String> Test() {
         Map<String, String> map = new HashMap<>();
@@ -48,6 +85,7 @@ public class MainController {
     }
 
 //    @CrossOrigin
+    @ResponseBody
     @PostMapping(value = "/testResult")
     public String TestResult(@RequestBody Map<String, Object> param){
         String str = (String)((Map)param.get("cc")).get("cc1");
