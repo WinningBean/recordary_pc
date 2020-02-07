aba8094f79e8b69787772f61ba41630fcb4081f0
sukyeongh <50071076+sukyeongh@users.noreply.github.com>
2020-02-07 11:26:01 +0900

    Revert "Revert "Merge branch 'sungho' into waterGlasses""
    This reverts commit d83e2c8e655b3477de03de096095f4d6d7540e46.

--- frontend/src/Components/Main/MainPage.js
+++ frontend/src/Components/Main/MainPage.js
@@ -41,38 +41,38 @@ class MainPage extends React.Component {
 
     async componentDidMount(){
         // 유저에 대한 정보를 가져오고 그 결과값을 state 에 저장
-        const {data} = await axios.get("http://localhost:8888/mainPage");
-    //     const data = {
-    //         currentUser:{
-    //             userEx: null,
-    //             userId: '1234',
-    //             userNm: '홍길동'
-    //     },
-    //     userFriend:[
-    //         {
-    //             friendCd:1,
-    //             friendNm:'친구1',
-    //             friendPic:'none'
-    //         },
-    //         {
-    //             friendCd:1,
-    //             friendNm:'친구2',
-    //             friendPic:'none'
-    //         }
-    //     ],
-    //     userGroup:[
-    //         {
-    //             groupCd:1,
-    //             groupNm:'그룹1',
-    //             groupPic:'none'
-    //         },
-    //         {
-    //             groupCd:2,
-    //             groupNm:'그룹2',
-    //             groupPic:'none'
-    //         }
-    //     ]
-    // }
+        // const {data} = await axios.get("http://localhost:8888/mainPage");
+        const data = {
+            currentUser:{
+                user_ex: null,
+                user_id: 'HelloWorld1234',
+                user_nm: '홍길동'
+        },
+        userFriend:[
+            {
+                friend_cd:1,
+                friend_nm:'친구1',
+                friend_pic:'http://placehold.it/40x40',
+            },
+            {
+                friend_cd:1,
+                friend_nm:'친구2',
+                friend_pic:'http://placehold.it/40x40',
+            }
+        ],
+        userGroup:[
+            {
+                group_cd:1,
+                group_nm:'그룹1',
+                group_pic:'http://placehold.it/40x40',
+            },
+            {
+                group_cd:2,
+                group_nm:'그룹2',
+                group_pic:'http://placehold.it/40x40',
+            }
+        ]
+    }
         this.setState({data, isLoading : false});
         console.log(data);
     }
