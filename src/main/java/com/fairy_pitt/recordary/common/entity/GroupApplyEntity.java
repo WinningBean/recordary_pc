11717843f47a92be623b98fc836edd91458894c3
jueun275 <52851721+jueun275@users.noreply.github.com>
2020-02-11 17:06:47 +0900

    create GroupApplyEntity

---
+++ src/main/java/com/fairy_pitt/recordary/common/entity/GroupApplyEntity.java
@@ -0,0 +1,30 @@
+package com.fairy_pitt.recordary.common.entity;
+
+import com.fairy_pitt.recordary.common.id.GroupMemberPK;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name="GROUP_APPLY_TB")
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(GroupMemberPK.class)
+public class GroupApplyEntity  implements Serializable {
+
+    @Id
+    @ManyToOne
+    private GroupEntity groupCodeFK;
+
+    @Id
+    @ManyToOne
+    private UserEntity userCodeFK;
+
+    private int applyState;
+
+
+}
