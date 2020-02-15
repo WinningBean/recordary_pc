075e77dd89c90d84d8bb5e2b5dbc9937dd5a2926
wsb0722 <wsb0722@naver.com>
2020-02-15 10:14:43 +0900

    Implement Post update

--- src/main/java/com/fairy_pitt/recordary/endpoint/post/PostController.java
+++ src/main/java/com/fairy_pitt/recordary/endpoint/post/PostController.java
@@ -1,25 +1,29 @@
 package com.fairy_pitt.recordary.endpoint.post;
 
 import com.fairy_pitt.recordary.common.entity.GroupEntity;
+import com.fairy_pitt.recordary.common.entity.PostEntity;
 import com.fairy_pitt.recordary.common.entity.UserEntity;
 import com.fairy_pitt.recordary.common.repository.GroupRepository;
+import com.fairy_pitt.recordary.common.repository.PostRepository;
 import com.fairy_pitt.recordary.common.repository.UserRepository;
 import com.fairy_pitt.recordary.endpoint.post.service.PostService;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("post")
 public class PostController {
     @Autowired private PostService postService;
+    @Autowired private HttpSession session;
+    @Autowired private PostRepository postRepository;
     @Autowired private UserRepository userRepository;
     @Autowired private GroupRepository groupRepository;
 
@@ -35,7 +39,23 @@ public class PostController {
         Boolean writeState = postService.create(userEntity, groupEntity, postMap);
 
         Map<String, Boolean> map = new HashMap<>();
-        map.put("is_write", writeState);
+        map.put("isWrite", writeState);
+        return map;
+    }
+
+    @PostMapping("/update")
+    public Map<String, Boolean> update(@RequestParam Map<String, Object> paramMap){
+        PostEntity postEntity = new PostEntity();
+        postEntity.setPostCd((Long)paramMap.get("post_cd"));
+        postEntity.setPostEx((String)paramMap.get("post_ex"));
+        postEntity.setPostPublicState((int)paramMap.get("post_pb_st"));
+
+        Boolean updateState = postService.update(postEntity);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("isUpdate", updateState);
+        return map;
+    }
+
         return map;
     }
 }
--- src/main/java/com/fairy_pitt/recordary/endpoint/post/service/PostService.java
+++ src/main/java/com/fairy_pitt/recordary/endpoint/post/service/PostService.java
@@ -1,18 +1,26 @@
 package com.fairy_pitt.recordary.endpoint.post.service;
 
+import com.fairy_pitt.recordary.common.entity.FollowerEntity;
 import com.fairy_pitt.recordary.common.entity.GroupEntity;
 import com.fairy_pitt.recordary.common.entity.PostEntity;
 import com.fairy_pitt.recordary.common.entity.UserEntity;
-import com.fairy_pitt.recordary.common.repository.PostRepository;
+import com.fairy_pitt.recordary.common.repository.*;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import javafx.scene.Group;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class PostService {
     @Autowired private PostRepository postRepository;
+    @Autowired private UserRepository userRepository;
+    @Autowired private FollowerRepository followerRepository;
+    @Autowired private GroupMemberRepository groupMemberRepository;
 
     public Boolean create(UserEntity currentUser, GroupEntity groupEntity, Map<String, Object> postMap){
         if (currentUser == null || postMap == null) return false;
@@ -30,4 +38,16 @@ public class PostService {
         if (resultPostEntity.isPresent()) return true;
         else return false;
     }
+
+    public Boolean update(PostEntity postEntity){
+        Long postCd = postEntity.getPostCd();
+        PostEntity updatePost = postRepository.findByPostCd(postCd);
+        if (updatePost == null) return false;
+
+        updatePost.setPostEx(postEntity.getPostEx());
+        updatePost.setPostPublicState(postEntity.getPostPublicState());
+        postRepository.save(updatePost);
+        return true;
+    }
+
 }
