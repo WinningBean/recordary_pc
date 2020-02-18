1fc124a347cd5bca7ab317c44fb529678046f8e8
Sungho-Wi <53851699+Sungho-Wi@users.noreply.github.com>
2020-02-19 00:06:19 +0900

    [frontend] Redux 라이브러리 도입 , Header에 Menu 부분 분할
    -Redux 사용해 모든 컴포넌트에 사용중인 react state중에서 여러 컴포넌트에서 사용하는 부분만 추출해 redux state에 부분저장
    -Header.js 에 Menu부분 HeaderMenu.js 로 분할 작업
    -CRUD시 즉시반영 이벤트 생성
    -Redux connect를 담을수있는 Containers 폴더 생성
    -HeaderMenu에 있던 버튼이 곂치는 에러 해결

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,260 +1,33 @@
 import React from 'react';
 import './header.css';
-import UserEditor from './UserEditor';
-import GroupAdd from '../Group/GroupAdd';
-import LongMenu from '../Other/MoreMenu';
-import ProfileEditor from 'Components/Profile/ProfileEditor';
-import GroupSetting from 'Components/Group/GroupSetting';
 
 import { styled } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
-import SettingsIcon from '@material-ui/icons/Settings';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import SearchAppBar from '../Other/SearchField';
-import ArrowDown from '@material-ui/icons/KeyboardArrowDown';
-import ArrowUp from '@material-ui/icons/KeyboardArrowUp';
-import AddIcon from '@material-ui/icons/Add';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import EditIcon from '@material-ui/icons/Edit';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import Button from '@material-ui/core/Button';
 
-const defaultProps = {
-    data : {
-        currentUser: {
-            user_ex: '',
-            user_id: 'HelloWorld1234',
-            user_nm: '홍길동'
-        },
-        userFriend: [
-            {
-                friend_cd: 1,
-                friend_nm: '친구1',
-                friend_pic: 'http://placehold.it/40x40',
-            },
-            {
-                friend_cd: 1,
-                friend_nm: '친구2',
-                friend_pic: 'http://placehold.it/40x40',
-            }
-        ],
-        userGroup: [
-            {
-                group_cd: 1,
-                group_nm: '그룹1',
-                group_pic: 'http://placehold.it/40x40',
-            },
-            {
-                group_cd: 2,
-                group_nm: '그룹2',
-                group_pic: 'http://placehold.it/40x40',
-            }
-        ],
-    }
-}
+import SearchAppBar from '../Other/SearchField';
+import HeaderMenu from 'Containers/Header/HeaderMenu';
+
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            menuClick: false,
-            ProfileEditorClick : false,
-            editorClick: false,
-            groupOpen: false,
-            groupAddClick : false,
-            friendOpen: false,
-            data: this.props.data,
-            menuDialog: null,
+            openMenu : false,
         }
     }
 
-    setGroupMenuOpen = () => {
-        if (this.state.groupOpen === true) {
-            return this.setState({
-                groupOpen: false
-            });
-        }
-        return this.setState({
-            groupOpen: true
-        });
-    };
-
-    setFriendMenuOpen = () => {
-        if (this.state.friendOpen === true) {
-            return this.setState({
-                friendOpen: false
-            });
-        }
-        return this.setState({
-            friendOpen: true
-        });
-    };
-
-    onGroupMenuSelect = (selectedValue, code) => {
-        switch(selectedValue){
-            case '그룹 정보':
-                break;
-            case '그룹 관리':
-                this.setState({menuDialog : <GroupSetting onClose={()=> this.setState({menuDialog : null})} />})
-                break;
-            case '그룹 삭제':
-                break;
-        }
-    }
-
-    componentDidMount(){
-        this.setState({data : this.props.data});
-    }
-    
     render() {
-        const GroupAddForm = () => {
-            if(this.state.groupAddClick === true){
-                return <GroupAdd onCancel={() => this.setState({ groupAddClick: false })}></GroupAdd>
-            }
-            return null;
-        }
-
-        const ProfileEditForm = () => {
-            if(this.state.ProfileEditorClick === true){
-                return <ProfileEditor onCancel={() => this.setState({ ProfileEditorClick: false })}></ProfileEditor>
-            }
-            return null;
-        }
-
-
-        const GroupList = (() => {
-            if (this.state.groupOpen === true) {
-                const gruops = this.state.data.userGroup.map(
-                    (value) => { return (
-                        <li key={value.group_cd}>
-                            <GroupButton>
-                                <div style={{ display: 'flex', width:'250px', justifyContent:'space-between' }}>
-                                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                                        <img alt="group-img" style={{ marginRight: '10px', borderRadius: '50%' }} src={value.group_pic} />
-                                        {value.group_nm}
-                                    </div>
-                                    <div className="LongMenuOpen">
-                                        <LongMenu 
-                                        options={[ '그룹 정보', '그룹 관리', '그룹 삭제']} 
-                                        style={{ fontSize: '30px' }}
-                                        code={value.group_cd}
-                                        returnValue={this.onGroupMenuSelect} 
-                                        />
-                                    </div>
-                                </div>
-                            </GroupButton>
-                        </li>)
-                    }
-                );
-                return (
-                    <div className="gruop-field">
-                        <GroupButton style={{backgroundColor:'rgba(209, 204, 192,0.4)'}}>
-                            <div>
-                                <span style={{fontSize:'18px', paddingTop: '5px'}}>Groups</span>
-                                <span>
-                                    <PlusIconButton><AddIcon onClick={()=> this.setState({groupAddClick: true})} style={{fontSize:'20px;'}}/></PlusIconButton>
-                                </span>
-                            </div>
-                            <span><IconButton><ArrowUp style={{fontSize:'30px'}}  onClick={this.setGroupMenuOpen}/></IconButton></span>
-                        </GroupButton>
-                        <div><ul>{gruops}</ul></div>
-                        
-                    </div>
-                );
-            }
-            return <GroupButton>
-                <div>
-                    <span style={{fontSize:'18px', paddingTop: '5px'}}>Groups</span>
-                    <span>
-                        <PlusIconButton><AddIcon style={{fontSize:'20px;'}} onClick={()=> this.setState({groupAddClick: true})} /></PlusIconButton>
-                    </span>
-                </div>
-                <span><IconButton><ArrowDown style={{ fontSize: '30px' }} onClick={this.setGroupMenuOpen}/></IconButton></span>
-            </GroupButton>;
-        })();
-        const friendList = (() => {
-            if (this.state.friendOpen === true) {
-                const friends = this.state.data.userFriend.map(
-                    (value) => { return (
-                    <li key={value.friend_cd}>
-                        <GroupButton>
-                            <div style={{display:'flex', alignItems:'center', marginBottom:'10px'}}>
-                                <img alt="friend-img" style={{marginRight:'10px', borderRadius:'50%'}} src={value.friend_pic} />
-                                {value.friend_nm}
-                            </div>
-                            <div>
-                                <IconButton><MoreVertIcon/></IconButton>
-                            </div>
-                        </GroupButton>
-                        </li>) }
-                );
-                return (
-                    <div className="gruop-field">
-                        <GroupButton style={{backgroundColor:'rgba(209, 204, 192,0.4)'}}>
-                            <div>
-                                <span style={{fontSize:'18px', paddingTop: '5px'}}>Friends</span>
-                            </div>
-                            <span><IconButton><ArrowUp style={{fontSize:'30px'}}  onClick={this.setFriendMenuOpen}/></IconButton></span>
-                        </GroupButton>
-                        <div><ul>{friends}</ul></div>
-                        
-                    </div>
-                );
-            }
-            return <GroupButton>
-                <div>
-                    <span style={{fontSize:'18px', paddingTop: '5px'}}>Friends</span>
-                </div>
-                <span><IconButton><ArrowDown style={{fontSize:'30px'}}  onClick={this.setFriendMenuOpen}/></IconButton></span>
-            </GroupButton>;
-        })();
-        const Editor = () => {
-            if (this.state.editorClick === true) {
-                return <UserEditor currentUser={this.state.data.currentUser} onCancel={() => this.setState({ editorClick: false })} />;
-            }
-            return null;
-        }
         return (
             <header>
                 <div id="header-left">
                     <div className="title-menu">
-                        <IconButton onClick={() => this.setState({ menuClick: true })}>
+                        <IconButton onClick={() => this.setState({ openMenu: true })}>
                             <MenuIcon style={{ fontSize: '30px', color: 'white'}} />
                         </IconButton>
-                        <Drawer
-                            open={this.state.menuClick}
-                            onClose={() => this.setState({ menuClick: false })}
-                            style={{backgroundColor:'rgba(241, 242, 246,0.1)'}}
-                            anchor='left'>
-                            <div className="menu-wrap">
-                                <div className="menu-profile">
-                                    <div className="menu-profile-pic-nm">
-                                        <div style={{marginRight:'10px'}}>
-                                            <img alt="user img" src="http://placehold.it/50x50" 
-                                            style={{borderRadius:'50%'}}
-                                            />
-                                        </div>
-                                        <span>{this.state.data.currentUser.user_id}</span>
-                                    </div>
-                                    <div className="profile-edit-icon">
-                                        <IconButton><EditIcon onClick={() => this.setState({ ProfileEditorClick: true })}/></IconButton>
-                                    </div>
-                                    {ProfileEditForm()}
-                                </div>
-                                <div className="menu-buttons">
-                                    {GroupList}
-                                    {friendList}
-                                </div>
-                                <div className="menu-bottom">
-                                    <IconButton onClick={() => this.setState({ editorClick: true })}><SettingsIcon /></IconButton>
-                                    <IconButton>로그아웃</IconButton>
-                                </div>
-                                {Editor()}
-                                {GroupAddForm()}
-                                {this.state.menuDialog}
-                            </div>
-                        </Drawer>
+                        <HeaderMenu open={this.state.openMenu} onClose={()=>this.setState({openMenu : false})} />
                     </div>
                     {/* <div className="title-icon">
                         <a href="profile.html"><img alt="icon" src="RIcon.png" /></a>
@@ -288,27 +61,10 @@ class Header extends React.Component {
     }
 }
 
-
 const IconButton = styled(Button)({
     minWidth: '40px',
     height: '40px',
 });
-const PlusIconButton = styled(Button)({
-    minWidth: '30px',
-    height: '40px',
-});
-const GroupButton = styled(Button)({
-    width: '250px',
-    height: '50px',
-    borderBottom: '1px solid rgba(209, 204, 192,0.8)',
-    borderRadius: '0',
-    display:'flex', 
-    justifyContent:'space-between',
-    paddingTop:'5px',
-    paddingBottom: '5px',
-    paddingLeft: '10px',
-});
-// export default withStyles(styles)(Header);
 
-Header.defaultProps = defaultProps;
+
 export default Header;
---
+++ frontend/src/Components/Header/HeaderMenu.js
@@ -0,0 +1,241 @@
+import React, { useState } from 'react';
+import './header.css';
+import UserEditor from './UserEditor';
+import GroupAdd from 'Containers/Group/GroupAdd';
+import LongMenu from '../Other/MoreMenu';
+import ProfileEditor from 'Components/Profile/ProfileEditor';
+import GroupSetting from 'Components/Group/GroupSetting';
+
+import { styled } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import ArrowUp from '@material-ui/icons/KeyboardArrowUp';
+import AddIcon from '@material-ui/icons/Add';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import EditIcon from '@material-ui/icons/Edit';
+
+const HeaderMenu = (props) => {
+    const data = props.data;
+    const [profileEditForm, setProfileEditForm] = useState(null);
+    const [editor, setEditor] = useState(null);
+    const [open, setOpen] = useState({
+        group: false,
+        friend: false,
+    });
+    const [groupAdd, setGroupAdd] = useState(null);
+    const [menuDialog, setMenuDialog] = useState(null);
+
+
+
+    const showOpen = (isGroup) => {
+        if (isGroup) {
+            setOpen({ ...open, group: !open.group });
+            return;
+        }
+        setOpen({ ...open, friend: !open.friend });
+        return;
+    };
+
+    const onGroupMenuSelect = (selectedValue, code) => {
+        const value = data.userGroup.filter((value)=>value.group_cd === code)[0];
+        // 그룹목록에서 현재 선택된 그룹 객체를 찾음
+        switch (selectedValue) {
+            case '그룹 정보':
+                break;
+            case '그룹 관리':
+                setMenuDialog(<GroupSetting data={value} onClose={() => setMenuDialog(null)} />);
+                break;
+            case '그룹 삭제':
+                break;
+        }
+    }
+
+    const showGroupAdd = () => {
+        if (groupAdd === null) {
+            setGroupAdd(<GroupAdd onCancel={()=>setGroupAdd(null)} />)
+            return;
+        }
+        setGroupAdd(null);
+        return;
+    }
+
+    const ShowProfileEditForm = () => {
+        if (profileEditForm === null) {
+            setProfileEditForm(<ProfileEditor onCancel={()=>setProfileEditForm(null)} />)
+            return;
+        }
+        setProfileEditForm(null)
+        return;
+    }
+
+    const GroupList = (() => {
+        if (open.group === true) {
+            const gruops = data.userGroup.map(
+                (value) => {
+                    return (
+                        <li key={value.group_cd}>
+                            <div className="button-wrap">
+                                <GroupButton>
+                                    <div style={{ display: 'flex', width: '250px', justifyContent: 'space-between' }}>
+                                        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                                            <img alt="group-img" style={{ marginRight: '10px', borderRadius: '50%' }} src={value.group_pic} />
+                                            {value.group_nm}
+                                        </div>
+                                        {/* <div className="LongMenuOpen">
+                                        <LongMenu
+                                            options={['그룹 정보', '그룹 관리', '그룹 삭제']}
+                                            code={value.group_cd}
+                                            returnValue={onGroupMenuSelect}
+                                        />
+                                    </div> */}
+                                    </div>
+                                </GroupButton>
+                                <div className="LongMenuOpen">
+                                    <LongMenu
+                                        options={['그룹 정보', '그룹 관리', '그룹 삭제']}
+                                        code={value.group_cd}
+                                        returnValue={onGroupMenuSelect}
+                                    />
+                                </div>
+                            </div>
+                        </li>)
+                }
+            );
+            return (
+                <div className="button-wrap">
+                    <GroupButton style={{ backgroundColor: 'rgba(209, 204, 192,0.4)' }} onClick={() => showOpen(true)}>
+                        <div>
+                            <span style={{ fontSize: '18px', paddingTop: '5px' }}>Groups</span>
+                        </div>
+                        <span><ArrowUp style={{ fontSize: '30px' }} /></span>
+                    </GroupButton>
+                    <div style={{ position: 'absolute', top: '4px', left: '85px' }}>
+                        <IconButton onClick={showGroupAdd}><AddIcon style={{ fontSize: '20px' }} /></IconButton>
+                    </div>
+                    <div><ul>{gruops}</ul></div>
+                </div>
+            );
+        }
+        return (<div className="button-wrap">
+            <GroupButton onClick={() => showOpen(true)}>
+                <div>
+                    <span style={{ fontSize: '18px', paddingTop: '5px' }}>Groups</span>
+                </div>
+                <span><ArrowDown style={{ fontSize: '30px' }} /></span>
+            </GroupButton>
+            <div style={{ position: 'absolute', top: '4px', left: '85px' }}>
+                <IconButton onClick={showGroupAdd}><AddIcon style={{ fontSize: '20px' }} /></IconButton>
+            </div>
+        </div>);
+    })();
+    const friendList = (() => {
+        if (open.friend === true) {
+            const friends = data.userFriend.map(
+                (value) => {
+                    return (
+                        <li key={value.friend_cd}>
+                            <GroupButton>
+                                <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                                    <img alt="friend-img" style={{ marginRight: '10px', borderRadius: '50%' }} src={value.friend_pic} />
+                                    {value.friend_nm}
+                                </div>
+                            </GroupButton>
+                            <div>
+                                <CustomIconButton><MoreVertIcon /></CustomIconButton>
+                            </div>
+                        </li>)
+                }
+            );
+            return (
+                <div className="gruop-field">
+                    <GroupButton style={{ backgroundColor: 'rgba(209, 204, 192,0.4)' }} onClick={() => showOpen(false)}>
+                        <div>
+                            <span style={{ fontSize: '18px', paddingTop: '5px' }}>Friends</span>
+                        </div>
+                        <span><ArrowUp style={{ fontSize: '30px' }} /></span>
+                    </GroupButton>
+                    <div><ul>{friends}</ul></div>
+                </div>
+            );
+        }
+        return <GroupButton onClick={() => showOpen(false)}>
+            <div>
+                <span style={{ fontSize: '18px', paddingTop: '5px' }}>Friends</span>
+            </div>
+            <span><ArrowDown style={{ fontSize: '30px' }} /></span>
+        </GroupButton>;
+    })();
+    const showEditor = () => {
+        if (editor === null) {
+            setEditor(<UserEditor currentUser={data.currentUser} onCancel={()=>setEditor(null)} />)
+            return;
+        }
+        setEditor(null);
+        return;
+    }
+
+    return (
+        <Drawer
+            open={props.open}
+            onClose={() => props.onClose()}
+            style={{ backgroundColor: 'rgba(241, 242, 246,0.1)' }}
+            anchor='left'>
+            <div className="menu-wrap">
+                <div className="menu-profile">
+                    <div className="menu-profile-pic-nm">
+                        <div style={{ marginRight: '10px' }}>
+                            <img alt="user img" src="http://placehold.it/50x50"
+                                style={{ borderRadius: '50%' }}
+                            />
+                        </div>
+                        <span>{data.currentUser.user_id}</span>
+                    </div>
+                    <div className="profile-edit-icon">
+                        <CustomIconButton onClick={ShowProfileEditForm}><EditIcon /></CustomIconButton>
+                    </div>
+                    {profileEditForm}
+                </div>
+                <div className="menu-buttons">
+                    {GroupList}
+                    {friendList}
+                </div>
+                <div className="menu-bottom">
+                    <CustomIconButton onClick={showEditor}><SettingsIcon /></CustomIconButton>
+                    <CustomIconButton>로그아웃</CustomIconButton>
+                </div>
+                {editor}
+                {groupAdd}
+                {menuDialog}
+            </div>
+        </Drawer>
+    )
+}
+
+
+const CustomIconButton = styled(Button)({
+    minWidth: '40px',
+    height: '40px',
+});
+const GroupButton = styled(Button)({
+    width: '250px',
+    height: '50px',
+    borderBottom: '1px solid rgba(209, 204, 192,0.8)',
+    borderRadius: '0',
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: '5px',
+    paddingBottom: '5px',
+    paddingLeft: '10px',
+});
+
+export default React.memo(
+    HeaderMenu,
+    (props, newProps) => {
+        console.log(props, newProps);
+        return props === newProps
+        // false 일시 랜더링
+        // true 일시 비랜더링
+    });
