8bcac8793c09c1bd10006afcb89042a83d16917b
jueun275 <52851721+jueun275@users.noreply.github.com>
2020-02-19 16:42:10 +0900

    Schedule Entity 생성

---
+++ src/main/java/com/fairy_pitt/recordary/common/entity/ScheduleEntity.java
@@ -0,0 +1,45 @@
+package com.fairy_pitt.recordary.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "Schedule_TB")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleEntity {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "SCHEDULE_CD")
+   private Long scheduleCd;
+
+   @ManyToOne
+   private PostEntity postCodeFK;
+
+   @ManyToOne
+   private ScheduleTabEntity TabCodeFK;
+
+   @Column(name = "SCHEDULE_NM")
+   private String scheduleNm;
+
+   @Column(name = "SCHEDULE_EX")
+   private String scheduleEx;
+
+   @Column(name = "SCHEDULE_STR_DT")
+   private Date scheduleStr;
+
+   @Column(name = "SCHEDULE_END_DT")
+   private Date scheduleEnd;
+
+   @Column(name = "SCHEDULE_COLOR")
+   private String scheduleCol;
+
+   //private String scheduleLocation;
+
+}
