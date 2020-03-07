6ed36218b8a29bd954b3f35d769472dacae830d8
sukyeongh <50071076+sukyeongh@users.noreply.github.com>
2020-03-07 10:33:05 +0900

    [frontend] 기타

--- frontend/src/Components/Other/MoreMenu.js
+++ frontend/src/Components/Other/MoreMenu.js
@@ -7,55 +7,52 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 const ITEM_HEIGHT = 48;
 
 export default function LongMenu(props) {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-    const handleClick = event => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const options = props.options;
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const options = props.options;
 
-    return (
-        <>
-            <IconButton
-                aria-label='more'
-                aria-controls='long-menu'
-                aria-haspopup='true'
-                onClick={handleClick}
-            >
-                <MoreVertIcon />
-            </IconButton>
-            <Menu
-                id='long-menu'
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
-                PaperProps={{
-                    style: {
-                        maxHeight: ITEM_HEIGHT * 4.5,
-                        width: 150
-                    }
-                }}
-            >
-                {options.map(option => (
-                    <MenuItem
-                        key={option}
-                        onClick={e => {
-                            props.returnValue(
-                                e.currentTarget.innerText,
-                                props.code
-                            );
-                        }}
-                    >
-                        {option}
-                    </MenuItem>
-                ))}
-            </Menu>
-        </>
-    );
+  return (
+    <>
+      <IconButton
+        aria-label='more'
+        aria-controls='long-menu'
+        aria-haspopup='true'
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id='long-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: 120
+          }
+        }}
+      >
+        {options.map(option => (
+          <MenuItem
+            key={option}
+            onClick={e => {
+              props.returnValue(e.currentTarget.innerText, props.code);
+            }}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
 }
