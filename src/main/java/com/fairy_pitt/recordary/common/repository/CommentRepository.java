4946ca66073157c2cf4cbda040da901c355f1628
jueun275 <52851721+jueun275@users.noreply.github.com>
2020-03-14 14:29:58 +0900

    Comment Repository

--- src/main/java/com/fairy_pitt/recordary/common/repository/CommentRepository.java
+++ src/main/java/com/fairy_pitt/recordary/common/repository/CommentRepository.java
@@ -6,6 +6,14 @@ import com.fairy_pitt.recordary.common.pk.FollowerPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+
+    Long countByCommentOriginFKIsNotNull();
+    Long countByCommentOriginFK(CommentEntity commentEntity);
+    List<CommentEntity> findAllByCommentOriginFK(CommentEntity commentEntity);
+    List<CommentEntity> findAllByCommentOriginFKNotNull();
+
 }
