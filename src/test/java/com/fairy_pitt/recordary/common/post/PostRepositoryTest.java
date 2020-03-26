57f27730ae606e1c5e695ceeefda9bffa95e06de
wsb0722 <wsb0722@naver.com>
2020-03-26 17:51:00 +0900

    Update Post test

--- src/test/java/com/fairy_pitt/recordary/common/post/PostRepositoryTest.java
+++ src/test/java/com/fairy_pitt/recordary/common/post/PostRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.fairy_pitt.recordary.common.post;
 
 import com.fairy_pitt.recordary.common.entity.PostEntity;
+import com.fairy_pitt.recordary.common.entity.UserEntity;
 import com.fairy_pitt.recordary.common.repository.PostRepository;
+import com.fairy_pitt.recordary.common.repository.UserRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,25 +18,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PostsRepositoryTest {
+public class PostRepositoryTest {
 
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @After
     public void cleanup() {
         postRepository.deleteAll();
+        postRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
     public void 게시글저장_불러오기() {
         //given
+        UserEntity user1 = userRepository.save(UserEntity.builder()
+                .userId("testUser1")
+                .userPw("testPassword")
+                .userNm("테스트 유저1")
+                .build());
+
         String postEx = "테스트 게시글";
         int postPublicState = 1;
         String postStrYMD = "20200310";
         String postEndYMD = "20200311";
 
         postRepository.save(PostEntity.builder()
+                .userFK(user1)
                 .postEx(postEx)
                 .postPublicState(postPublicState)
                 .postStrYMD(postStrYMD)
@@ -46,6 +59,7 @@ public class PostsRepositoryTest {
 
         //then
         PostEntity postEntity = postEntityList.get(0);
+        assertThat(postEntity.getUserFK().getUserId()).isEqualTo(user1.getUserId());
         assertThat(postEntity.getPostEx()).isEqualTo(postEx);
         assertThat(postEntity.getPostPublicState()).isEqualTo(postPublicState);
         assertThat(postEntity.getPostStrYMD()).isEqualTo(postStrYMD);
