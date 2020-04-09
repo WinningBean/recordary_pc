62fd6fdbb1c3132424117f1096f8f0357d0f6ab2
wsb0722 <wsb0722@naver.com>
2020-04-09 10:54:57 +0900

    Update Controllers

--- src/main/java/com/fairy_pitt/recordary/endpoint/main/MainController.java
+++ src/main/java/com/fairy_pitt/recordary/endpoint/main/MainController.java
@@ -1,28 +1,10 @@
 package com.fairy_pitt.recordary.endpoint.main;
 
-import com.fairy_pitt.recordary.endpoint.follower.service.FollowerService;
-import com.fairy_pitt.recordary.endpoint.group.service.GroupMemberService;
-import com.fairy_pitt.recordary.endpoint.group.service.GroupService;
-import com.fairy_pitt.recordary.endpoint.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
-import javax.servlet.http.HttpSession;
-import javax.transaction.Transactional;
-
-@Transactional
+@RequiredArgsConstructor
 @Controller
 public class MainController {
-
-    @Autowired
-    private HttpSession session;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private FollowerService followerService;
-    @Autowired
-    private GroupService groupService;
-    @Autowired
-    private GroupMemberService groupmemberService;
 
 }
