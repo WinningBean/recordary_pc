6bf341b6d1be880ed27644d1e62e0b8aa1a5618f
Sungho-Wi <53851699+Sungho-Wi@users.noreply.github.com>
2020-05-18 13:32:43 +0900

    [frontend] 일정 추가 컴포넌트 구체화

--- frontend/src/Components/UI/PublicRange.js
+++ frontend/src/Components/UI/PublicRange.js
@@ -7,16 +7,21 @@ import PublicIcon from '@material-ui/icons/Public';
 
 const options = ['전체공개', '나만보기', '친구만', '팔로워만'];
 
-export default function PublicRange() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
+// onSetSelectedIndex={(index) => setInfo({ ...info, schedulePublicState: index })}
+// state 변경 함수 props
+// selectedIndex={info.schedulePublicState}
+// 해당 index state
 
-  const handleClickListItem = event => {
+export default function PublicRange({ onSetSelectedIndex, selectedIndex }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  // const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleClickListItem = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
   const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
+    onSetSelectedIndex(index);
     setAnchorEl(null);
   };
 
@@ -29,23 +34,15 @@ export default function PublicRange() {
       <div className='plus-button-design' onClick={handleClickListItem}>
         <div className='plus-button-design-2'>
           <PublicIcon style={{ fontSize: '30px' }} />
-          <span style={{ fontSize: '15px', marginLeft: '5px' }}>
-            {options[selectedIndex]}
-          </span>
+          <span style={{ fontSize: '15px', marginLeft: '5px' }}>{options[selectedIndex]}</span>
         </div>
       </div>
-      <Menu
-        id='lock-menu'
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
+      <Menu id='lock-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
         {options.map((option, index) => (
           <MenuItem
             key={option}
             selected={index === selectedIndex}
-            onClick={event => handleMenuItemClick(event, index)}
+            onClick={(event) => handleMenuItemClick(event, index)}
             onChange={handleClose}
           >
             {option}
