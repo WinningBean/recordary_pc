38a9c0ab0edcd8b31c3bd86d73ae759674632f55
jueun275 <52851721+jueun275@users.noreply.github.com>
2020-06-08 14:34:10 +0900

    Create Todo

---
+++ src/main/java/com/fairy_pitt/recordary/endpoint/toDo/ToDoController.java
@@ -0,0 +1,41 @@
+package com.fairy_pitt.recordary.endpoint.toDo;
+
+import com.fairy_pitt.recordary.endpoint.toDo.dto.ToDoResponseDto;
+import com.fairy_pitt.recordary.endpoint.toDo.dto.TodoRequestDto;
+import com.fairy_pitt.recordary.endpoint.toDo.service.ToDoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("toDo")
+@RequiredArgsConstructor
+@RestController
+public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    @PostMapping("/")
+    public Boolean create(@RequestBody TodoRequestDto requestDto){
+        return toDoService.create(requestDto);
+    }
+
+    @PostMapping("update/{id}")
+    public Boolean update(@PathVariable Long id)
+    {
+       return toDoService.update(id);
+    }
+
+    @GetMapping("{userCd}")
+    public List<ToDoResponseDto> getCurrTodoList(@PathVariable Long userCd)
+    {
+        return toDoService.getCurrTodoList(userCd);
+    }
+
+    @GetMapping("pre/{userCd}")
+    public List<ToDoResponseDto> getPreTodoList(@PathVariable Long userCd)
+    {
+        return toDoService.getPreTodoList(userCd);
+    }
+
+}
---
+++ src/main/java/com/fairy_pitt/recordary/endpoint/toDo/dto/ToDoResponseDto.java
@@ -0,0 +1,25 @@
+package com.fairy_pitt.recordary.endpoint.toDo.dto;
+
+import com.fairy_pitt.recordary.common.entity.ToDoEntity;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class ToDoResponseDto {
+
+    private Long toDoCd;
+    private String toDoContent;
+    private String  toDoCol;
+    private Date toDoEndDate;
+    private Boolean toDoCompleteState;
+
+    public ToDoResponseDto(ToDoEntity entity)
+    {
+        this.toDoCd = entity.getToDoCd();
+        this.toDoCol = entity.getToDoCol();
+        this.toDoCompleteState = entity.getToDoCompleteState();
+        this.toDoContent = entity.getToDoContent();
+        this.toDoEndDate = entity.getToDoEndDate();
+    }
+}
