0c9d7e74a962e5844276608d20f744a4f6436659
wsb0722 <wsb0722@naver.com>
2020-07-04 13:58:09 +0900

    Add checkSessionLogout

--- src/main/java/com/fairy_pitt/recordary/endpoint/chat/ChatController.java
+++ src/main/java/com/fairy_pitt/recordary/endpoint/chat/ChatController.java
@@ -2,15 +2,8 @@ package com.fairy_pitt.recordary.endpoint.chat;
 
 import com.fairy_pitt.recordary.endpoint.chat.dto.ChatDto;
 import com.fairy_pitt.recordary.endpoint.chat.service.ChatService;
+import com.fairy_pitt.recordary.endpoint.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatService chatService;
+    private final UserService userService;
 
     @PostMapping("chat/sendMessage")
     public Long sendMessage(@RequestBody ChatDto incoming)
     {
+        userService.checkSessionLogout();
         Long chatCd =  chatService.create(incoming);
         chatService.stomp(chatCd, incoming.getRoomCd());
         return chatCd;
